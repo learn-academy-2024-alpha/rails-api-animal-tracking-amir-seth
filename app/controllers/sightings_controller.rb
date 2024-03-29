78a14b351ac00fd0e40a8d6e8d8fccf367d9e531
sethcrist <sethcrist50@gmail.com>
2024-03-29 16:48:10 -0700

    finished story 3

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,6 @@
 class SightingsController < ApplicationController
-    def create
-        sightings = Sighting.all
+    def index
+        sightings = Sighting.where(date: params[:start_date]..params[:end_date])
         render json: sightings
     end
 
@@ -18,9 +18,19 @@ class SightingsController < ApplicationController
         sighting = Sighting.find(params[:id])
         sighting.update(sighting_params)
         render json: sighting
+    end
+
+    def destroy
+        sighting = Sighting.find(params[:id])
+        sighting.destroy
+        render json: sighting
+    end
+
 
     private
     def sighting_params
-        params.require(:sighting).permit(:longitude, :latitude, :date, :animal_id)
+        params.require(:sighting).permit(:start_date, :end_date, :longitude, :latitude, :date, :animal_id)
     end
 end
+
+
